b666061bf972d050a0c3c11d5af692e40c2bc48c
truongtv <truongg9655@gmail.com>
2026-04-16 14:20:41 +0700

    remove spinner in NProgress

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import NProgress from 'nprogress'
 import './index.css'
 import 'nprogress/nprogress.css'
 import App from './App.tsx'
 import { setupInterceptors } from './services/setupInterceptors'
 
+NProgress.configure({ showSpinner: false })
 setupInterceptors()
 
 createRoot(document.getElementById('root')!).render(
